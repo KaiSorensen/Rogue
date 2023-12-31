e548c7d00aac8e5d0bae1f897b02d92eb8299a8d
Kai Sorensen <kylersoren@gmail.com>
2023-12-30 23:14:18 -0800

    more ground work done

--- stamp.c
+++ stamp.c
@@ -32,10 +32,8 @@ WAVHeader newWAVHeader(FILE* f)
 {
     WAVHeader newHead;
     fread(newHead.riff, 4, 1, f); newHead.riff[4] = '\0';
-    printf("riff = %s\n", newHead.riff);
     fread(&newHead.chuckSize, 4, 1, f);
     fread(newHead.wave, 4, 1, f); newHead.wave[4] = '\0';
-    printf("wave = %s\n", newHead.wave);
     fread(newHead.subChunk1ID, 4, 1, f); newHead.subChunk1ID[4] = '\0';
     fread(&newHead.subChunk1Size, 4, 1, f);
     fread(&newHead.audioFormat, 2, 1, f);
@@ -64,21 +62,45 @@ int isWAV(FILE* f)
 //finds a point at which the audio files align, that way
 //Note: this works best if, when you activate the microphones, you clap TWICE loudly with the microphones next to each other
 //returns the amount of time to cut off the first audio file (might be negative, in which case cut the second audio file)
-int syncAudio(FILE* a1, FILE* a2, int beginSecond, int endSecond)
+double syncAudio(FILE* a1, FILE* a2, int beginSecond, int endSecond)
 {
     WAVHeader h1 = newWAVHeader(a1);
     WAVHeader h2 = newWAVHeader(a2);
 
-    int offset; //return value
-    //audio spike indices, set to the first chunk of the spike, since a spike might last multiple chunks
-    int s1a1; //first spike of a1
-    int s2a1; //second spike of a1
-    int s1a2; //first spike of a2
-    int s2a2; //second spike of a2
+    //calculating begin and end bytes in which to check for double claps
+    int startByte = 44 + (beginSecond * h1.sampleRate * (h1.bitsPerSample/8));
+    int endByte = 44 + (endSecond * h1.sampleRate * (h1.bitsPerSample/8));
 
+    //USAGE
+    if(endSecond - beginSecond <= 0) {printf("You're stupid."); return 0;}
+    if(endByte > (h1.subChunk2Size + 36)) {printf("endSecond is past the end of file1"); return 0;}
+    if(endByte > (h2.subChunk2Size + 36)) {printf("endSecond is past the end of file2"); return 0;}
+
+    double offset; //return value
+    //audio spike indices, set to the first chunk of the spike, since a spike might last multiple chunks
+    double s1a1; //first spike of a1
+    double s2a1; //second spike of a1
+    double s1a2; //first spike of a2
+    double s2a2; //second spike of a2
+
+    //used for finding the biggest spikes
+    double previousMax = 0;
+    double runningTotal = 0;
+
+    //the core functionality comes next, two loops, one for each file
+    fseek(a1, startByte, SEEK_SET);
+    while (fread() == ) {
+
+    }
+
+    fseek(a2, startByte, SEEK_SET);
     while (0) {
 
     }
+
+    //debugging
+    printf("In file1, claps were %f seconds apart.", s2a1 - s1a1);
+    printf("In file2, claps were %f seconds apart.", s2a2 - s1a2);
 
     return offset;
 }
@@ -105,7 +127,7 @@ int main(int argc, char *argv[])
     fseek(a2, 0, SEEK_SET);
 
     //pray this works first try
-    printf("AUDIO SYNC AT: %d", syncAudio(a1,a2,0,30));
+    printf("AUDIO SYNC AT: %f", syncAudio(a1,a2,0,30));
 
     //GOODBYE
     fclose(a1);
