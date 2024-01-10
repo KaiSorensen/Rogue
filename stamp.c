8dc89a3e75bb59a5a880fd57edc242c69808d2c5
Kai <92950455+KaiSorensen@users.noreply.github.com>
2024-01-09 20:55:38 -0700

    Update stamp.c

--- stamp.c
+++ stamp.c
@@ -22,7 +22,7 @@ typedef struct WAVHeader {
     int sampleRate;
     int byteRate;
     short blockAlign;
-    short bitsPerSample;
+    short bytesPerSample;
     char data[5];
     int dataSize;
 } WAVHeader;
@@ -31,7 +31,7 @@ typedef struct WAVHeader {
 WAVHeader newWAVHeader(FILE* f)
 {
     WAVHeader newHead;
-    
+
     fread(newHead.riff, 4, 1, f); newHead.riff[4] = '\0';
     fread(&newHead.chuckSize, 4, 1, f);
     fread(newHead.wave, 4, 1, f); newHead.wave[4] = '\0';
@@ -50,7 +50,7 @@ WAVHeader newWAVHeader(FILE* f)
     fread(&newHead.sampleRate, 4, 1, f);
     fread(&newHead.byteRate, 4, 1, f);
     fread(&newHead.blockAlign, 2, 1, f);
-    fread(&newHead.bitsPerSample, 2, 1, f);
+    fread(&newHead.bytesPerSample, 2, 1, f); newHead.bytesPerSample /= 8;
 
     //finds the data section
     fread(newHead.data, 4, 1, f); newHead.data[4] = '\0';
@@ -71,10 +71,10 @@ char* wavHeaderToString(WAVHeader *header) {
     int requiredLength = snprintf(NULL, 0,
                                 "RIFF: %.4s\nChunk Size: %d\nWAVE: %.4s\nSubchunk1 ID: %.4s\nSubchunk1 Size: %d\n"
                                 "Audio Format: %hd\nNum Channels: %hd\nSample Rate: %d\nByte Rate: %d\n"
-                                "Block Align: %hd\nBits Per Sample: %hd\nSubchunk2 ID: %.4s\nSubchunk2 Size: %d\n",
+                                "Block Align: %hd\nBytes Per Sample: %hd\nSubchunk2 ID: %.4s\nSubchunk2 Size: %d\n",
                                 header->riff, header->chuckSize, header->wave, header->fmt, header->fmtSize,
                                 header->audioFormat, header->numChannels, header->sampleRate, header->byteRate,
-                                header->blockAlign, header->bitsPerSample, header->data, header->dataSize);
+                                header->blockAlign, header->bytesPerSample, header->data, header->dataSize);
 
     // Allocate memory (+1 for the null terminator)
     char *str = malloc(requiredLength + 1);
@@ -87,10 +87,10 @@ char* wavHeaderToString(WAVHeader *header) {
     snprintf(str, requiredLength + 1,
              "RIFF: %.4s\nChunk Size: %d\nWAVE: %.4s\nSubchunk1 ID: %.4s\nSubchunk1 Size: %d\n"
              "Audio Format: %hd\nNum Channels: %hd\nSample Rate: %d\nByte Rate: %d\n"
-             "Block Align: %hd\nBits Per Sample: %hd\nSubchunk2 ID: %.4s\nSubchunk2 Size: %d\n",
+             "Block Align: %hd\nBytes Per Sample: %hd\nSubchunk2 ID: %.4s\nSubchunk2 Size: %d\n",
              header->riff, header->chuckSize, header->wave, header->fmt, header->fmtSize,
              header->audioFormat, header->numChannels, header->sampleRate, header->byteRate,
-             header->blockAlign, header->bitsPerSample, header->data, header->dataSize);
+             header->blockAlign, header->bytesPerSample, header->data, header->dataSize);
 
     return str;
 }
@@ -107,17 +107,32 @@ int isWAV(FILE* f)
     else return 0;
 }
 
-//finds a point at which the audio files align, that way
-//Note: this works best if, when you activate the microphones, you clap TWICE loudly with the microphones next to each other
+//for testing, written by GPT-4
+int reverseBits(int n) {
+    int reversed = 0;
+    for (int i = 0; i < 32; i++) {
+        reversed = (reversed << 1) | (n & 1);
+        n >>= 1;
+    }
+    return reversed;
+}
+
+//finds a point at which the audio files align, based on the TWO-CLAP technique
+//Note: When you activate the microphones, you clap TWICE loudly with the microphones next to each other
 //returns the amount of time to cut off the first audio file (might be negative, in which case cut the second audio file)
 double syncAudio(FILE* a1, FILE* a2, int beginSecond, int endSecond)
 {
     WAVHeader h1 = newWAVHeader(a1);
     WAVHeader h2 = newWAVHeader(a2);
+    printf("\n%s\n", wavHeaderToString(&h1));
+    printf("\n%s\n", wavHeaderToString(&h2));
 
     //calculating begin and end bytes in which to check for double claps
-    int startByte = 44 + (beginSecond * h1.sampleRate * (h1.bitsPerSample/8));
-    int endByte = 44 + (endSecond * h1.sampleRate * (h1.bitsPerSample/8));
+    int startByte = ftell(a1) + (beginSecond * h1.sampleRate * (h1.bytesPerSample));
+    int endByte = ftell(a1) + (endSecond * h1.sampleRate * (h1.bytesPerSample));
+    printf ("\nA1: %d\n", ftell(a1));
+    printf ("\nStart Byte: %d\n", startByte);
+    printf ("End Byte: %d\n", endByte);
 
     //USAGE
     if(endSecond - beginSecond <= 0) {printf("You're stupid."); return 0;}
@@ -125,33 +140,81 @@ double syncAudio(FILE* a1, FILE* a2, int beginSecond, int endSecond)
     if(endByte > (h2.dataSize + 36)) {printf("endSecond is past the end of file2"); return 0;}
 
     double offset; //return value
-    //audio spike indices, set to the first chunk of the spike, since a spike might last multiple chunks
-    double s1a1; //first spike of a1
-    double s2a1; //second spike of a1
-    double s1a2; //first spike of a2
-    double s2a2; //second spike of a2
+    //audio spike indices, set to the first sample of the spike, since a spike might last multiple chunks
+    int s1a1 = startByte; //first spike of a1
+    int s2a1 = startByte; //second spike of a1
+    int s1a2 = startByte; //first spike of a2
+    int s2a2 = startByte; //second spike of a2
 
     //used for finding the biggest spikes
-    int currentSample;
+    int previousSample;
+    signed int currentSample;
     int previousMax = 0;
+    int firstMax = 0;
+    int secondMax = 0;
     int runningTotal = 0;
 
     //the core functionality comes next, two loops, one for each file
+
+    //finds the two claps in a1
     fseek(a1, startByte, SEEK_SET);
     while (ftell(a1) < endByte) {
-
-        //fread(currentSample, h1.chuckSize, 1, a1);
+        previousSample = currentSample;
+        fread(&currentSample, h1.bytesPerSample, 1, a1);
+        printf("\nCurrentByte: %d    CurrentSample: %d", ftell(a1), currentSample);
+        if(currentSample >= previousSample) {
+            runningTotal += currentSample - previousSample;
+        } else {
+            if(runningTotal > secondMax) {
+                firstMax = secondMax;
+                secondMax = runningTotal;
+                runningTotal = 0;
+                s1a1 = s2a2;
+                s1a1 = ftell(a1);
+            } else if (runningTotal > firstMax){
+                firstMax = runningTotal;
+                runningTotal = 0;
+                s2a1 = ftell(a1);
+            }
+        }
     }
 
+    previousMax = 0;
+    firstMax = 0;
+    secondMax = 0;
+    runningTotal = 0;
+    //finds the two claps in a2
     fseek(a2, startByte, SEEK_SET);
     while (ftell(a2) < endByte) {
-
-        //fread(currentSample, h2.chuckSize, 1, a1);
+        previousSample = currentSample;
+        fread(&currentSample, h2.bytesPerSample, 1, a2);
+        printf("\nCurrentByte: %d    CurrentSample: %d", ftell(a2), currentSample);
+        if(currentSample >= previousSample) {
+            runningTotal += currentSample - previousSample;
+        } else {
+            if(runningTotal > secondMax) {
+                firstMax = secondMax;
+                secondMax = runningTotal;
+                runningTotal = 0;
+                s1a1 = s2a2;
+                s1a1 = ftell(a1);
+            } else if (runningTotal > firstMax){
+                firstMax = runningTotal;
+                runningTotal = 0;
+                s2a1 = ftell(a1);
+            }
+        }
     }
 
     //debugging
-    printf("In file1, claps were %f seconds apart.", s2a1 - s1a1);
-    printf("In file2, claps were %f seconds apart.", s2a2 - s1a2);
+    printf("\n\nIn file 1, claps were %d bytes apart.\n", s2a1 - s1a1);
+    printf("In file 2, claps were %d bytes apart.\n", s2a2 - s1a2);
+
+    if(abs((s2a1-s1a1) - (s1a2 - s2a2)) < 100) {
+        printf("FOUND THE CLAPS IN BOTH FILES\n");
+    } else {
+        
+    }  
 
     return offset;
 }
@@ -163,7 +226,7 @@ int createStamps(FILE* a1, FILE* a2, double startStamp)
     
 }
 
-// "Is they Jacob's? Don't lie to me mayne." -Kanye
+//  "Is they Jacob's? Don't lie to me mayne." -Kanye
 int main(int argc, char *argv[]) 
 {
     //USAGE
@@ -177,13 +240,13 @@ int main(int argc, char *argv[])
     fseek(a1, 0, SEEK_SET);
     fseek(a2, 0, SEEK_SET);
 
-    //pray this works first try... NOPE
-    WAVHeader h1 = newWAVHeader(a1);
-    char *s = wavHeaderToString(&h1);
-    printf("\n%s\n", s);
-    free(s);
+    //testing
+    // WAVHeader h1 = newWAVHeader(a1);
+    // char *s = wavHeaderToString(&h1);
+    // printf("\n%s\n", s);
+    // free(s);
 
-    //printf("AUDIO SYNC AT: %f", syncAudio(a1,a2,0,30));
+    printf("AUDIO SYNC AT: %f\n", syncAudio(a1,a2,0,1));
 
     //GOODBYE
     fclose(a1);
