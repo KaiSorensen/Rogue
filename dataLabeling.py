abb54363e843c0007e68a50a42de8a1380aae383
Kai Sorensen <kylersoren@gmail.com>
2024-02-05 00:40:36 -0700

    switching to python

---
+++ dataLabeling.py
@@ -0,0 +1,7 @@
+'''
+This program will provide an interface for labeling training data.
+The idea is to create a file that contains timestamps that the user creates.
+The user listens to the audio and presses a button upon each voice switch.
+Labeling data takes work, and I won't be able to have a program that labels data until start here.
+'''
+
